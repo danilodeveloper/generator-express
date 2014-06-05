0f9155829cb25ac66399554a04cf5b857d99ad98
danilodeveloper <mcbarsotti@gmail.com>
2014-06-04 23:43:09 -0300

    express updated included new dependencies in package.json (basic and mvc) app.configure call removed from express.js according to migration guide router removed
    Midlewares included (on express.js file):
    morgan
    body-parser
    compression
    serve-favicon
    method-override

--- app/templates/mvc/config/express.js
+++ app/templates/mvc/config/express.js
@@ -1,8 +1,13 @@
-var express = require('express');
+var express = require('express'),
+    compress = require('compression'),
+    favicon = require('serve-favicon'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser')
+    methodOverride = require('method-override');
 
 module.exports = function(app, config) {
   app.configure(function () {
-    app.use(express.compress());
+    app.use(compress());
     app.use(express.static(config.root + '/public'));
     app.set('port', config.port);
     app.set('views', config.root + '/app/views');
@@ -10,11 +15,10 @@ module.exports = function(app, config) {
         app.engine('ejs', require('ejs-locals'));
     <% } %>
     app.set('view engine', '<%= options.viewEngine %>');
-    app.use(express.favicon(config.root + '/public/img/favicon.ico'));
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
+    app.use(favicon(config.root + '/public/img/favicon.ico'));
+    app.use(morgan());
+    app.use(bodyParser());
+    app.use(methodOverride());
     app.use(function (req, res) {
       res.status(404).render('404', { title: '404' });
     });
